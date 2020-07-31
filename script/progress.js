66aa58fa988eb69f7694b7e3abfdb67e2b6ea145
Maíra <mairatabata@outlook.com>
2020-07-31 13:50:28 -0300

    refactor(progress-bar): rename class

--- script/progress.js
+++ script/progress.js
@@ -1,4 +1,4 @@
-const bar = document.querySelector('.bar');
+const bar = document.querySelector('.progress__bar');
 
 export const updateProgressBar = ({ percent }) => {
   bar.style.width = `${Math.round(percent) || 0}%`;
