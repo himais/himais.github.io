006fec13734f47bd91e1086a6f9cfb33924f5108
Maíra <mairatabata@outlook.com>
2020-08-06 17:36:56 -0300

    refactor(all): format code

--- script/libs/index.js
+++ script/libs/index.js
@@ -3,14 +3,14 @@ import { updateProgressBar } from '../components/progress.js';
 // Init third-party libraries
 const initLibraries = () => {
   // Pagination and menu
-  new Pageable("#main", {
-    childSelector: "[data-anchor]",
+  const pageable = new Pageable('#main', {
+    childSelector: '[data-anchor]',
     anchors: ['Page 1 ', 'Page 2', 'Page 3', 'Page 4'],
     pips: true,
     animation: 300,
     delay: 0,
     throttle: 0,
-    orientation: "vertical",
+    orientation: 'vertical',
     events: {
       wheel: true,
       mouse: true,
