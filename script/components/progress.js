467a73e57dde5a22e42794d08671342364ebb633
Maíra <mairatabata@outlook.com>
2020-08-07 00:59:20 -0300

    all new, all good 🎉

--- script/components/progress.js
+++
@@ -1,5 +0,0 @@
-const bar = document.querySelector('.progress__bar');
-
-export const updateProgressBar = ({ percent }) => {
-  bar.style.width = `${Math.round(percent) || 0}%`;
-}
--- script/libs/index.js
+++ script/libs/index.js
@@ -1,28 +1,5 @@
-import { updateProgressBar } from '../components/progress.js';
-
 // Init third-party libraries
 const initLibraries = () => {
-  // Pagination and menu
-  const pageable = new Pageable('#main', {
-    childSelector: '[data-anchor]',
-    anchors: ['Page 1 ', 'Page 2', 'Page 3', 'Page 4'],
-    pips: true,
-    animation: 300,
-    delay: 0,
-    throttle: 0,
-    orientation: 'vertical',
-    events: {
-      wheel: true,
-      mouse: true,
-      touch: true,
-      keydown: true,
-    },
-    onScroll: updateProgressBar,
-  });
-
-  // Parallax effect
-  new Rellax('.parallax');
-
   // Particle effect on hero section
   particlesJS.load('particles', 'config/particles.json');
 };
