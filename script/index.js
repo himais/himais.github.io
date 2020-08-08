8bc7dd55341b0cb8d8e38387a45eb233102f12d8
Maíra <mairatabata@outlook.com>
2020-08-07 22:33:47 -0300

    fix(particle-background): remove container on mobile screen

--- script/index.js
+++ script/index.js
@@ -4,6 +4,12 @@ import initSections from './sections/index.js';
 const app = async () => {
   initLibraries();
   initSections();
+
+  // Temporary fix. Waiting for solution from for naker.io.
+  if (window.matchMedia('(max-width: 992px)').matches) {
+    var elem = document.querySelector('.bg');
+    elem.parentNode.removeChild(elem);
+  }
 };
 
 document.addEventListener('DOMContentLoaded', app);
